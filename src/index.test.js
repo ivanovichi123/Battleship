840723efe349d2c4d5287018229ea729073baf28
ivanovichi <ivan.arthur18@gmail.com>
2025-11-25 08:44:46 -0600

    !feat: Gameboard function added
    The function still has a lot of bugs

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,137 @@
-import { sum } from "./index";
+import { Ship } from "./ship";
+import { Gameboard } from "./gameboard";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+const theShip1 = new Ship(3, 0, false);
+const theShip2 = new Ship(2, 0, false);
+const theGameboard = new Gameboard();
+
+test("The ship 1 receives a hit", () => {
+  theShip1.hit();
+  expect(theShip1.getTBH).toBe(1);
 });
+
+test("The ship 2 receives a hit", () => {
+  theShip2.hit();
+  expect(theShip2);
+});
+
+test("The ship 1 is sunk", () => {
+  theShip1.hit();
+  theShip1.hit();
+  expect(theShip1.getSunk).toBe(true);
+});
+
+test("The ship 2 is sunk", () => {
+  theShip2.hit();
+  expect(theShip2.getSunk).toBe(true);
+});
+
+const theShip3 = new Ship(3, 0, false);
+const theShip4 = new Ship(2, 0, false);
+const theShip5 = new Ship(4, 0, false);
+const theShip6 = new Ship(3, 0, false);
+
+test("The Ship 3 in coordinate start [5,5] and final [5,3]", () => {
+  theGameboard.placeShip(theShip3, [5, 5], [5, 3]);
+  let theGrid = theGameboard.getGrid;
+  expect(theGrid[5][5].getLength).toBe(3);
+  expect(theGrid[5][4].getLength).toBe(3);
+  expect(theGrid[5][3].getLength).toBe(3);
+});
+
+test("The Ship 4 in coordinate start [3,3] and final [2,3]", () => {
+  theGameboard.placeShip(theShip4, [3, 3], [2, 3]);
+  let theGrid = theGameboard.getGrid;
+  expect(theGrid[3][3].getLength).toBe(2);
+  expect(theGrid[2][3].getLength).toBe(2);
+});
+
+test("The Ship 5 coordinates are smaller than his length in start [0,10] and final [1,10]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [0,10], [1,10])).toThrow(Error);
+});
+
+test("The Ship 5 in coordinate start [1,4] and final [-2,4] cannot be possible", () => {
+  expect(() => theGameboard.placeShip(theShip5, [1, 4], [-2, 4])).toThrow(Error);
+});
+
+test("The Ship 5 can not be put diagonally in coordinate start [7,7] and final [10,10]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [7, 7], [10, 10])).toThrow(Error);
+});
+
+test("The Ship 5 can not be put diagonally in coordinate start [7,7] and final [8,6]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [7, 7], [8, 6])).toThrow(Error);
+});
+
+test("The Ship 5 can not be put diagonally in coordinate start [7,7] and final [6,8]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [7, 7], [6, 8])).toThrow(Error);
+});
+
+test("The Ship 5 can not be put diagonally in coordinate start [9,7] and final [7,5]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [9, 7], [7, 5])).toThrow(Error);
+});
+
+test("The Ship 5 can not be put diagonally in coordinate start [0,0] and final [3,3]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [0, 0], [3, 3])).toThrow(Error);
+});
+
+
+test("The Ship 5 can not be put in random coordinates start [3,4] and final [7,4]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [3,4], [7,4])).toThrow(Error);
+})
+
+test("The Ship 5 can not be put in coordinates already in use in coordinate start [1,3] and final [4,3]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [1, 3], [4, 3])).toThrow(Error);
+});
+
+test("The Ship 5 error of putting him in an existing coordinate erase the already put pointers", () => {
+  let theGrid = theGameboard.getGrid;
+  expect(theGrid[1][3]).toBe(0);
+  expect(theGrid[2][3].getLength).toBe(2);
+  expect(theGrid[3][3].getLength).toBe(2);
+  expect(theGrid[4][3]).toBe(0);
+});
+
+test("The Ship 5 can not be put in coordinates already in use in coordinate start [5,7] and final [5,4]", () => {
+  expect(() => theGameboard.placeShip(theShip5, [5, 7], [5, 4])).toThrow(Error);
+});
+
+test("The Ship 5 error of putting him in an existing coordinate erase the already put pointers", () => {
+  let theGrid = theGameboard.getGrid;
+  expect(theGrid[5][7]).toBe(0);
+  expect(theGrid[5][6]).toBe(2);
+  expect(theGrid[5][5].getLength).toBe(3);
+  expect(theGrid[5][4].getLength).toBe(3);
+});
+
+
+
+// test("The Ship 5 in coordinate start [3,1] and final [6,1]", () => {
+//   theGameboard.placeShip(theShip5,[3,1],[6,1]);
+//   let theGrid = theGameboard.getGrid;
+//   expect(theGrid[3][1]).toBe(3);
+//   expect(theGrid[4][1]).toBe(3);
+//   expect(theGrid[5][1]).toBe(3);
+//   expect(theGrid[6][1]).toBe(3);
+// });
+
+// test("The Ship 6 can not be put in coordinates already in use in coordinate start [5,5] and final [5,5]", () => {
+//   expect(() => theGameboard.placeShip(theShip5, [2, 5], [2, 8])).toThrow(Error);
+// });
+
+// test("The Ship 6 error of putting him in an existing coordinate erase the already put pointers", () => {
+//   let theGrid = theGameboard.getGrid;
+//   expect(theGrid[2][5]).toBe(0);
+//   expect(theGrid[2][6]).toBe(2);
+//   expect(theGrid[2][7].getLength).toBe(3);
+//   expect(theGrid[2][8].getLength).toBe(3);
+// });
+
+
+// test("The Ship 6 in coordinate start [2,7] and final [2,9]", () => {
+//   theGameboard.placeShip(theShip6,[3,1],[6,1]);
+//   let theGrid = theGameboard.getGrid;
+//   expect(theGrid[3][1]).toBe(3);
+//   expect(theGrid[4][1]).toBe(3);
+//   expect(theGrid[5][1]).toBe(3);
+//   expect(theGrid[6][1]).toBe(3);
+// });
